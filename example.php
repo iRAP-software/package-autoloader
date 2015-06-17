def6ddfbc2d457f0e1327649debb755ebf0f54ea
programster <sdpagent@gmail.com>
2015-06-17 10:41:32 +0100

    initial commit

---
+++ example.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once(__DIR__ . '/vendor/autoload.php');
+
+$sid = '';
+$auth_token = '';
+$directories = array(
+    __DIR__,
+    __DIR__ . '/controllers',
+    __DIR__ . '/models',
+    __DIR__ . '/views',
+);
+
+$autoloader = new \iRAP\Autoloader\Autoloader($directories);
+
+# do nothing with the $autoloader, everything was run in the constructor
